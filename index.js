0916e614e83b6072cce595a112963642145417dc
Taylor Sanders <tsanders3515@gmail.com>
2021-02-22 20:28:23 -0900

    adding name property to questions

--- index.js
+++ index.js
@@ -8,32 +8,39 @@ const questions = () =>
     inquirer.prompt([
         {
             type: 'input',
+            name: 'github',
             message: "What is your GitHub username?",
         },
         {
             type: 'input',
+            name: 'email',
             message: "What is your email address?",
         },
         {
             type: 'input',
+            name: 'project',
             message: "What is your project's name?",
         },
         {
             type: 'input',
+            name: 'description',
             message: "Please write a short description of your project",
         },
         {
             type: 'input',
+            name: 'license',
             message: "What kind of license should your project have?",
             choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
         },
         {
             type: 'input',
+            name: 'dependencies',
             message: "What command should be run to install dependencies?",
             default: "npm i",
         },
         {
             type: 'input',
+            name: 'tests',
             message: "What command should be run to run tests?",
             default: "npm test",
         },
@@ -43,6 +50,7 @@ const questions = () =>
         },
         {
             type: 'input',
+            name: 'repo',
             message: "What does the user need to know about contributing to the repo?",
         }
     ]);
