bbde80bc482aed4120e0578232d32ab19bd26e0a
Taylor Sanders <tsanders3515@gmail.com>
2021-02-23 11:33:59 -0900

    generating markdown working

---
+++ README1.md
@@ -0,0 +1,29 @@
+# taylor repo
+
+    ## About The Project
+
+    # description here
+
+## Getting Started
+To get a local copy up and running follow these simple steps. You can also download the source files provided. You will need a text editor such as Visual Studio Code, Xcode or similar to edit the source code.
+
+### Installation
+
+# *Clone this repo: taylor to your local device. Use npm i to install dependencies.
+
+## Usage
+        
+# information about usage
+
+*To test this project run npm test
+
+## License
+
+# MIT
+
+## Contact
+
+# email address
+
+*To contribute to this project contribute here
+
--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const questions = () =>
             message: "Please write a short description of your project",
         },
         {
-            type: 'input',
+            type: 'checkbox',
             name: 'license',
             message: "What kind of license should your project have?",
             choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
@@ -46,6 +46,7 @@ const questions = () =>
         },
         {
             type: 'input',
+            name: 'user',
             message: "What does the user need to know about using the repo?"
         },
         {
@@ -55,25 +56,79 @@ const questions = () =>
         }
     ]);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFileSync(fileName, data, err => {
-        if (err) {
-            return console.log(err);
+const generateMarkdown = (answers) =>
+    `
+    http://img.shelds.io${answers.github}/${answers.project}
+    
+    # ${answers.project}
+
+    ## About The Project
+
+    # ${answers.description}
+
+## Getting Started
+To get a local copy up and running follow these simple steps. You can also download the source files provided. You will need a text editor such as Visual Studio Code, Xcode or similar to edit the source code.
+
+### Installation
+
+# *Clone this repo: ${answers.github} to your local device. Use ${answers.dependencies} to install dependencies.
+
+## Usage
+        
+# ${answers.user}
+
+*To test this project run ${answers.tests}
+
+## License
+
+# ${answers.license[0]}
+
+## Contact
+
+# ${answers.email}
+
+*To contribute to this project ${answers.repo}
+
+`
+
+const init = () => {
+    questions().then((answers) => {
+        try {
+            const md = generateMarkdown(answers);
+            fs.writeFileSync('README1.md', md);
+            console.log('Successfully wrote to README1.md');
+        } catch (error) {
+            console.log(error);
         }
-        // console.log('Success! Your file has been generated!');
     });
+};
 
-    const writeFile = inquirer.promisify(writeToFile);
-
-    // TODO: Create a function to initialize app
-    function init() {
-        questions.then(responses => {
-            //console.log('the answer is: ', responses.questions)
-        })
-    }
+init();
 
 
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+//     fs.writeFileSync(fileName, data, err => {
+//         if (err) {
+//             return console.log(err);
+//         }
+//         // console.log('Success! Your file has been generated!');
+//     });
+// }
 
-    // Function call to initialize app
-    init()
+// const writeFile = inquirer.promisify(writeToFile);
+
+// // TODO: Create a function to initialize app
+// function init() {
+//     questions.then(responses => {
+//         //console.log('the answer is: ', responses.questions)
+//     })
+// }
+
+
+
+// // Function call to initialize app
+// questions().then((answers => {
+//     console.log(answers)
+// }))
+
